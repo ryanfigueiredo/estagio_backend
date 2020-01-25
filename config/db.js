0659bc92096d472669fae685aed1cbbd7dde0276
César Ryan <43640672+ryanfigueiredo@users.noreply.github.com>
2020-01-25 17:06:54 -0300

    Delete db.js

--- config/db.js
+++
@@ -1,5 +0,0 @@
-if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb://user:password@ds259154.mlab.com:59154/tarefas" }
-}else{
-    module.exports = {mongoURI: "mongodb://localhost/estagio"}
-}
